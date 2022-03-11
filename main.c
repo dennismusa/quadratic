264df7341dc5eb9c23b9a8a2fbddc70eb9e78735
dennismusa <97958664+dennismusa@users.noreply.github.com>
2022-03-11 17:38:28 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+/*
+quadratic
+By DENNIS MUSA
+Last updated FEB2022
+C89 compiler
+MIT license
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Variable declaration
+    char name [100];
+    float quot;
+    printf("value of x!\n");
+
+    //input
+    printf("Enter quadratic\n");
+    gets("x");
+    printf("value x,b");
+    printf("Enter quadratic equation!\n");
+
+    //computation
+    if("b-4ac=0") {
+        Equation has two equal values;
+    else if("b-4ac>0");
+        Equation has two distinct values;
+    else if(b-4ac<0);
+        Equation has no roots;
+    }
+}   printf("value of x\n");
+     return 0;
